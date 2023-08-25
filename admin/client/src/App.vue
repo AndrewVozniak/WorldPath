efea29767160182beda25bc86e0c7bbc2092eba5
Andrew Vozniak | Oria <a.vozniaks@gmail.com>
2023-08-25 23:35:25 +0300

    Added vue js

---
+++ admin/client/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import SidebarComponent from "@/components/SidebarComponent.vue";
+</script>
+
+<template>
+  <div class="main-container">
+    <SidebarComponent></SidebarComponent>
+
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.main-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.content {
+  width: 90%;
+
+  padding-top: 15px;
+  padding-left: 30px;
+}
+</style>
---
+++ admin/client/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+let title = 'WorldPath Dashbaord';
+</script>
+
+<template>
+  <div class="homepage">
+    <h1 class="title">{{ title }}</h1>
+  </div>
+</template>
+
+<style>
+.homepage {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.title {
+  font-size: 2rem;
+  font-weight: 700;
+  margin-bottom: 1rem;
+}
+</style>
