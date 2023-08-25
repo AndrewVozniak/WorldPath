5fc472a77ff3f48f35f4bcc83b32854d122ee967
Andrew Vozniak | Oria <a.vozniaks@gmail.com>
2023-08-25 22:31:55 +0300

    Added sidebar

--- admin/client/src/app/components/sidebar/sidebar.component.ts
+++ admin/client/src/app/components/sidebar/sidebar.component.ts
@@ -5,6 +5,45 @@ import { Component } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
 
+export class SidebarComponent {
+  pages = [
+    {
+      title: 'Dashboard',
+      link: '/'
+    },
+    {
+      title: 'Users',
+      link: '/users'
+    },
+    {
+      title: 'Community',
+      link: '/community'
+    },
+    {
+      title: 'Reviews',
+      link: '/reviews'
+    },
+    {
+      title: 'Places',
+      link: '/places'
+    },
+    {
+      title: 'Trips',
+      link: '/trips'
+    },
+    {
+      title: 'Routes',
+      link: '/routes'
+    },
+    {
+      title: 'Weather',
+      link: '/weather'
+    },
+    {
+      title: 'Support',
+      link: '/support'
+    }
+  ]
 }
+
