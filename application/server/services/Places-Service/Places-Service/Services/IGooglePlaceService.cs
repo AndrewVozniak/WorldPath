6cc2bc9001968acce90118c549a324f817808717
eeestrany <yaroslavprotsyk310@gmail.com>
2023-08-26 21:48:51 +0300

    Extract the logic from the controller.

---
+++ application/server/services/Places-Service/Places-Service/Services/IGooglePlaceService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Places_Service.Models;
+
+namespace Places_Service.Services;
+
+public interface IGooglePlaceService
+{
+    Task<PlacesApiResponse> GetPlaceByCoordinate(float lat, float lon, int radius = 1000);
+}
