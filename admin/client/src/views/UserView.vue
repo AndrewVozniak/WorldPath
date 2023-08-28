a0a49008233bf8dde083d07666e08bc7a2f10ab9
Andrew Vozniak | Oria <a.vozniaks@gmail.com>
2023-08-28 23:23:10 +0300

    Added part of user view in admin

--- admin/client/src/views/UserView.vue
+++ admin/client/src/views/UserView.vue
@@ -19,10 +19,20 @@ getUser();
 
 <template>
   <div class="page">
-    <div class="content">
-      <h1 class="title">{{ user.name }}</h1>
-      <img :src="user.profile_photo_path" :alt="user.profile_photo_path">
+    <div class="content" v-if="user">
+      <RouterLink to="/users" class="page_link">To all users...</RouterLink>
 
+      <h1 class="title">{{ user.name }}</h1>
+
+      <div class="user__top__info">
+        <img :src="user.profile_photo_path" :alt="user.name" class="user-photo">
+
+        <div class="user__info__data">
+          <span>Email verified at: {{ user.email_verified_at }}</span>
+          <span>Updated at: {{ user.updated_at }}</span>
+          <span>Created at: {{ user.created_at }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,6 +44,41 @@ getUser();
   margin-left: auto;
   margin-right: auto;
 
+  .page_link {
+    color: #4b3e3e;
+    margin-bottom: 20px;
 
+    transition: all 0.3s;
+
+    &:hover {
+      color: #4d8d28;
+    }
+  }
+
+  .user__top__info {
+    display: flex;
+    align-items: start;
+    justify-content: space-between;
+
+    margin-top: 20px;
+
+    .user-photo {
+      width: 150px;
+      height: 150px;
+      border-radius: 50%;
+    }
+
+    .user__info__data {
+      display: flex;
+      flex-direction: column;
+      font-weight: 600;
+
+      text-align: end;
+
+      span {
+        margin-bottom: 10px;
+      }
+    }
+  }
 }
 </style>
