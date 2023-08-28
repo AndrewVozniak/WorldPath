5e295318ec9d7d0975910e92a5c4dd8b70ba7bfd
eeestrany <yaroslavprotsyk310@gmail.com>
2023-08-28 20:37:55 +0300

    Added the functionality of commenting places.

--- application/server/services/Places-Service/Places-Service/Controllers/PlacesController.cs
+++ application/server/services/Places-Service/Places-Service/Controllers/PlacesController.cs
@@ -112,6 +112,26 @@ namespace Places_Service.Controllers
 
             return Ok(placeLike);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Comment([FromBody] PlaceCommentDto commentDto)
+        {
+            var placeExists = await _context.Places.AnyAsync(p => p.Id == commentDto.PlaceId);
+
+            if (!placeExists) return NotFound("The specified PlaceId does not exist.");
+            
+            var placeComment = new PlaceComment
+            {
+                PlaceId = commentDto.PlaceId,
+                UserId = commentDto.UserId,
+                Text = commentDto.Text,
+            };
+
+            await _context.PlaceComments.AddAsync(placeComment);
+            await _context.SaveChangesAsync();
+
+            return Ok(placeComment);
+        }
     }
 }    
             
--- application/server/services/Places-Service/Places-Service/Dtos/PlaceCommentDto.cs
+++ application/server/services/Places-Service/Places-Service/Dtos/PlaceCommentDto.cs
@@ -1,6 +1,13 @@
-﻿namespace Places_Service.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Places_Service.Dtos;
 
 public class PlaceCommentDto
 {
-    
+    [Required] 
+    public int UserId { get; set; }
+    [Required]
+    public int PlaceId { get; set; }
+    [Required]
+    public string Text { get; set; }
 }
