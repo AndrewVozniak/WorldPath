8b43ea0551e495174793c422cdfe117969044e1c
Andrew Vozniak | Oria <BrainStormDen@gmail.com>
2023-09-05 15:58:21 +0300

    Added errors to search component in weather page

--- application/client/web/src/app/components/weather-search-heading/weather-search-heading.component.ts
+++ application/client/web/src/app/components/weather-search-heading/weather-search-heading.component.ts
@@ -9,8 +9,43 @@ import {OnInit} from "@angular/core";
 export class WeatherSearchHeadingComponent {
   @Output() changeHoursToDisplayEvent = new EventEmitter<number>();
 
+  protected city: string = '';
+  protected lat: string = '';
+  protected lon: string = '';
+  protected error: string = '';
+
   public ngOnInit(): void {
     this.changeHoursToDisplay(120);
+  }
+
+  onCityFieldChange(event: any): void {
+    this.city = event.target.value;
+  }
+
+  onLatFieldChange(event: any): void {
+    this.lat = event.target.value;
+  }
+
+  onLonFieldChange(event: any): void {
+    this.lon = event.target.value;
+  }
+
+  public searchByCity(): void {
+    if(!this.city) {
+      this.error = 'City name is required';
+    }
+  }
+
+  public searchByCoordinates(): void {
+    if(!this.lat) {
+      this.error = 'Latitude are required';
+    }
+
+    if(!this.lon) {
+      this.error = 'Longitude are required';
+    }
+
+
   }
 
   public changeHoursToDisplay(hoursToDisplay: number): void {
--- application/client/web/src/app/pages/weather/weather.component.ts
+++ application/client/web/src/app/pages/weather/weather.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
