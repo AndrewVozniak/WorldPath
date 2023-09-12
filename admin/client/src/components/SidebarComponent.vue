7ffabff5a59b04d3c2c4e1337c46d9505b02097b
Andrew Vozniak | Oria <BrainStormDen@gmail.com>
2023-09-13 01:21:49 +0300

    Visual + changed links to dynamic ( now using array and v-for )

--- admin/client/src/components/SidebarComponent.vue
+++ admin/client/src/components/SidebarComponent.vue
@@ -3,6 +3,18 @@ import {ref} from "vue";
 import Button from "@/components/ui/button/Button.vue";
 
 const username = ref(localStorage.getItem('username'));
+
+const links = [
+  { name: 'Dashboard', path: '/' },
+  { name: 'Users', path: '/users' },
+  { name: 'Community', path: '/community' },
+  { name: 'Reviews', path: '/reviews' },
+  { name: 'Places', path: '/places' },
+  { name: 'Trips', path: '/trips' },
+  { name: 'Routes', path: '/routes' },
+  { name: 'Weather', path: '/weather' },
+  { name: 'Support', path: '/support' },
+];
 </script>
 
 <template>
@@ -11,15 +23,9 @@ const username = ref(localStorage.getItem('username'));
     <h2 class="text-center text-white font-semibold">Welcome to the admin panel</h2>
 
     <div class="flex flex-col mt-10">
-      <RouterLink to="/"><Button variant="secondary" class="w-full mb-5">Dashboard</Button></RouterLink>
-      <RouterLink to="/users"><Button variant="secondary" class="w-full mb-5">Users</Button></RouterLink>
-      <RouterLink to="/community"><Button variant="secondary" class="w-full mb-5">Community</Button></RouterLink>
-      <RouterLink to="/reviews"><Button variant="secondary" class="w-full mb-5">Reviews</Button></RouterLink>
-      <RouterLink to="/places"><Button variant="secondary" class="w-full mb-5">Places</Button></RouterLink>
-      <RouterLink to="/trips"><Button variant="secondary" class="w-full mb-5">Trips</Button></RouterLink>
-      <RouterLink to="/routes"><Button variant="secondary" class="w-full mb-5">Routes</Button></RouterLink>
-      <RouterLink to="/weather"><Button variant="secondary" class="w-full mb-5">Weather</Button></RouterLink>
-      <RouterLink to="/support"><Button variant="secondary" class="w-full mb-5">Support</Button></RouterLink>
+      <RouterLink :to="link.path" v-for="link in links" :key="link.name">
+        <Button variant="secondary" class="w-full mb-5 text-md font-semibold">{{ link.name }}</Button>
+      </RouterLink>
     </div>
   </div>
 </template>
