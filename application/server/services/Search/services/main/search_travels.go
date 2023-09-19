c13161e7c3328251dd35eb33d2fbcd2c42d55662
Andrew Vozniak | Oria <BrainStormDen@gmail.com>
2023-09-19 12:45:19 +0300

    Added search by rabbit in travels

--- application/server/services/Search/services/main/search_travels.go
+++ application/server/services/Search/services/main/search_travels.go
@@ -9,7 +9,7 @@ func SearchTravels(req string) string {
 	client := rpc.NewClient(travels.RabbitUser, travels.RabbitPass, travels.RabbitHost, travels.RabbitPort)
 	defer client.Close()
 
-	response := client.SendRequest("search", req)
+	response := client.SendRequest("search_travel_by_name", req)
 
 	return response
 }
