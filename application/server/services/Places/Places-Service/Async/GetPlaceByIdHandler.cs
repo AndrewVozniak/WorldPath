95e556d8fe52da88bca1bc3f864c0e740a0726ad
Yaroslav <yaroslavprotsyk310@gmail.com>
2023-09-20 19:34:15 +0300

    Created Handlers for RabbitMq

---
+++ application/server/services/Places/Places-Service/Async/GetPlaceByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using Newtonsoft.Json;
+using Places_Service.Dtos;
+
+namespace Places_Service.Async;
+
+public class GetPlaceByIdHandler
+{
+    [Obsolete("Obsolete")]
+    public void HandleMessage(byte[] body)
+    {
+        // Створіть потік для читання з байтового масиву
+        using (MemoryStream memoryStream = new MemoryStream(body))
+        {
+            // Створіть BinaryFormatter для десеріалізації
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            // Десеріалізуйте байтовий масив у об'єкт PlaceDto
+            PlaceDto placeDto = (PlaceDto)binaryFormatter.Deserialize(memoryStream);
+
+            // Тепер ви можете використовувати об'єкт placeDto
+            
+            
+        }
+    }
+}
---
+++ application/server/services/Places/Places-Service/Async/RabbitMQMessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Places_Service.Controllers;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Places_Service.Async;
+
+public class RabbitMqMessageHandler
+{
+    private readonly IConfiguration _configuration;
+    private readonly PlacesController _placesController;
+    private readonly IModel _rabbitMqChannel;
+    private readonly IConnection _connection;
+
+    public RabbitMqMessageHandler(PlacesController placesController, IModel rabbitMqChannel, IConfiguration configuration)
+    {
+        _placesController = placesController;
+        _rabbitMqChannel = rabbitMqChannel;
+        _configuration = configuration;
+
+        var factory = new ConnectionFactory()
+        {
+            HostName = _configuration["RabbitMQHost"],
+            Port = int.Parse(_configuration["RabbitMQPort"]!)
+        };
+        
+        _connection = factory.CreateConnection();
+        _rabbitMqChannel = _connection.CreateModel();
+    }
+    
+    public void HandleMessages()
+    {
+        _rabbitMqChannel.QueueDeclare("getPlace_byName", false, false, false, null);
+        _rabbitMqChannel.QueueDeclare("getPlace_byId", false, false, false, null);
+    }
+}
