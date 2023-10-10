b154f46a5a73295e4106dbf655c504bb47cffc0a
eeestrany <yaroslavprotsyk310@gmail.com>
2023-10-10 06:19:04 +0300

    Added Domain layer

---
+++ application/server/services/Places/Places.Domain/ParsedPlacePhoto.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Places.Domain;
+
+public class ParsedPlacePhoto
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    
+    [BsonElement("PlaceId")]
+    [JsonPropertyName("placeId")]
+    public string? PlaceId { get; set; }
+    
+    [BsonElement("PhotoPath")]
+    [JsonPropertyName("photoPath")]
+    public string? PhotoPath { get; set; }
+    
+    [BsonElement("UpdatedAt")]
+    [JsonPropertyName("updatedAt")]
+    public DateTime? UpdatedAt { get; set; }
+    
+    [BsonElement("CreatedAt")]
+    [JsonPropertyName("createdAt")]
+    public DateTime CreatedAt { get; set; }
+}
---
+++ application/server/services/Places/Places.Domain/PlaceLike.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Places.Domain;
+
+public class PlaceLike
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    [BsonElement("UserId")]
+    [JsonPropertyName("UserId")]
+    public string? UserId { get; set; }
+    [BsonElement("PlaceId")]
+    [JsonPropertyName("PlaceId")]
+    public string? PlaceId { get; set; }
+    
+    [BsonElement("UpdatedAt")]
+    [JsonPropertyName("UpdatedAt")]
+    public DateTime? UpdatedAt { get; set; }
+    
+    [BsonElement("CreatedAt")]
+    [JsonPropertyName("CreatedAt")]
+    public DateTime CreatedAt { get; set; }
+}
